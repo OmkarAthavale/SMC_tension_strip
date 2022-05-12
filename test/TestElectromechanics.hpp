47e7fa6e8ef162da77838e8880e231ea68063ab7
Omkar Athavale <oa1234@live.com>
2022-05-12 22:49:33 +0000

    fixed combiningvectors

--- test/TestElectromechanics.hpp
+++ test/TestElectromechanics.hpp
@@ -31,24 +31,19 @@ public:
             = NonlinearElasticityTools<2>::GetNodesByComponentValue(mechanics_mesh, 0, 0.0); // all the X=0.0 nodes
         std::vector<unsigned> fixed_nodes_RHS
             = NonlinearElasticityTools<2>::GetNodesByComponentValue(mechanics_mesh, 0, 0.9); // all the X=0.0 nodes
-        TRACE("abc1")
         // set fixed nodes
         std::vector<unsigned> fixed_nodes;
         std::vector<unsigned>::iterator it, st;
-        TRACE("abc2")
 
         std::sort(fixed_nodes_LHS.begin(), fixed_nodes_LHS.end());
         std::sort(fixed_nodes_RHS.begin(), fixed_nodes_RHS.end());
-        TRACE("abc3")
 
-        it = std::set_union(fixed_nodes_LHS.begin(), fixed_nodes_LHS.end(),fixed_nodes_RHS.begin(), fixed_nodes_RHS.end(), fixed_nodes.begin());
-        TRACE("abc4")
+        std::set_union(fixed_nodes_LHS.begin(), fixed_nodes_LHS.end(),fixed_nodes_RHS.begin(), fixed_nodes_RHS.end(), std::back_inserter(fixed_nodes));
+        
+        for (st = fixed_nodes.begin(); st != fixed_nodes.end(); ++st) std::cout << ' ' << *st;
+        std::cout << '\n';       
 
-        for (st = fixed_nodes.begin(); st != it; ++st) std::cout << ' ' << *st;
-        std::cout << '\n';
-
-        TRACE("abc5")
-
+        
         ElectroMechanicsProblemDefinition<2> problem_defn(mechanics_mesh);
         problem_defn.SetContractionModel(KERCHOFFS2003,0.01/*contraction model ODE timestep*/);
         problem_defn.SetUseDefaultCardiacMaterialLaw(INCOMPRESSIBLE);
