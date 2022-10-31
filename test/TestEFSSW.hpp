f2eb97ae4ecef9ff97be41f2924a2cdbf12dd204
Omkar Athavale <oath399@UoA.auckland.ac.nz>
2022-11-01 09:31:03 +1300

    extended duration

--- test/TestEFSSW.hpp
+++ test/TestEFSSW.hpp
@@ -93,8 +93,8 @@ class TestEFS : public CxxTest::TestSuite
     std::string output_dir = mesh_ident + "-BaselineCheckpoint";
     unsigned bath_attr = 0;
     unsigned icc_attr = 1;
-    double duration = 10.0;      // ms
-    double print_step = 5.0;        // ms
+    double duration = 60000.0;      // ms
+    double print_step = 100.0;        // ms
     // ---------------------------------------- //
 
     // Mesh location
